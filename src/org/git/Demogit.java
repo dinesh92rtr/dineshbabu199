462aa0cbbdce0998d62f92b3719f508bce307d6d
dinesh92rtr <dinesshasokan10@gmail.com>
2021-12-13 20:33:01 -0800

    arun task done

--- src/org/git/Demogit.java
+++ src/org/git/Demogit.java
@@ -5,8 +5,14 @@ public class Demogit {
 	public void add() {
 		System.out.println("add");
 	}
+	
 	public static void main(String[] args) {
 		Demogit d=new Demogit();
 		d.add();
+	}
+	
+	public void sub() {
+		System.out.println("sub");
+
 	}
 }
