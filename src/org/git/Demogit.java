06a15fdb672e7e5a3d03b1e2be9841f4ff9262e9
dinesh92rtr <dinesshasokan10@gmail.com>
2021-12-13 19:34:23 -0800

    task completed

---
+++ src/org/git/Demogit.java
@@ -0,0 +1,12 @@
+package org.git;
+
+public class Demogit {
+
+	public void add() {
+		System.out.println("add");
+	}
+	public static void main(String[] args) {
+		Demogit d=new Demogit();
+		d.add();
+	}
+}
